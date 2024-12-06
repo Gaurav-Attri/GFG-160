a7e142c8ca35ffb6b0770d803dbe74a3bccb458d
Gaurav-Attri <attrigaurav60@gmail.com>
2024-12-06 18:32:25 +0530

    Day 7: Stock Buy and Sell: Maximum Transactions allowed Solution

---
+++ Arrays/Day-7/StockBuyAndSellMulTransactions.java
@@ -0,0 +1,35 @@
+/**
+ * OPTIMAL
+ * TC - O(n)
+ * SC - O(1)
+ */
+class Solution {
+    public int maximumProfit(int prices[]) {
+        int n = prices.length;
+        if(n == 1) return 0;
+        
+        int maxProfit = 0;
+        int previousPrice = -1;
+        
+        for(int i = 0; i < n; i++){
+            int currentPrice = prices[i];
+            if(i == n-1){
+                if(previousPrice != -1 && currentPrice > previousPrice){
+                    maxProfit += currentPrice - previousPrice;
+                }
+                break;
+            }
+            int nextPrice = prices[i+1];
+            
+            if(currentPrice > nextPrice && previousPrice != -1){
+                maxProfit += currentPrice - previousPrice;
+                previousPrice = -1;
+            }
+            else if(currentPrice <= nextPrice && previousPrice == -1){
+                previousPrice = currentPrice;
+            }
+        }
+        
+        return maxProfit;
+    }
+}
