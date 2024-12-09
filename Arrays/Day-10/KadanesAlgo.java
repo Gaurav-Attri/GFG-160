78006d6729792e03861984beef5c2c5925e2f564
Gaurav-Attri <attrigaurav60@gmail.com>
2024-12-09 20:00:35 +0530

    Day 10: Kadane's Algorithm Solution

---
+++ Arrays/Day-10/KadanesAlgo.java
@@ -0,0 +1,16 @@
+class Solution {
+    int maxSubarraySum(int[] arr) {
+
+        int res = arr[0];
+        int maxEnding = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            
+            maxEnding = Math.max(maxEnding + arr[i], arr[i]);
+          
+            res = Math.max(res, maxEnding);
+        }
+        
+        return res;
+    }
+}
